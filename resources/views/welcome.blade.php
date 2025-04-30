a76c34068a88ea256f6d8603d403ef0691a2585a
junior67205 <bourdonjunior@gmail.com>
2025-04-30 21:36:32 +0200

    Mise en place de laravel et tailwindCSS

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        @vite('resources/css/app.css')
+        <!-- Fonts -->
+
+        <!-- Styles / Scripts -->
+       
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold underline">
+            Hello World
+        </h1>
+    </body>
+</html>
